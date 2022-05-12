6f312d4cf402d5bdb96cd40fcc55925e3eed8f09
DanielK2001 <daniel.kathiresan@gmail.com>
2022-05-12 23:16:15 +1200

    Basic changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import axios from 'axios';
 const App = function () {
 	return (
 		<>
-			<h1>Speed Project</h1>
+			<h1 class="centertitle">Speed Project</h1>
+			<div>
+			<button class="customButton" href="FindArticle.js">Find Article</button>
+			<button class="customButton"><a href="SubmitArticle.js">Submit Article</a></button>
+			</div>
 		</>
 	);
 };
---
+++ src/SubmitArticle.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+const App = function () {
+	return (
+		<>
+			<h1 class="centertitle">Submit Article</h1>
+			<div>
+			<button class="customButton" href="App.js">Home</button>
+			</div>
+		</>
+	);
+};
+export default App
