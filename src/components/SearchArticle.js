0cf76fabcccf373960517b50f6574a957b337751
Daniel Kathiresan <daniel.kathiresan@gmail.com>
2022-06-05 23:55:05 +1200

    Changed moderator login

--- src/components/SearchArticle.js
+++ src/components/SearchArticle.js
@@ -90,7 +90,7 @@ class SearchArticle extends Component {
         <a class="active" href="/search-article">Search Article</a>
         <a href="#about">About</a>
         <div class="topnav-right">
-          <a href="/moderator-login" >Moderator Access</a>
+        <a href="/moderator-panel" >Moderator Access</a>
         </div>
       </div>
         <div className="container">
